d2b6179dd2eac974abdff945a167f9d2a9a61c69
prem kumar <prem.kumar@sastechstudio.com>
2026-04-14 19:25:57 +0530

    comming test api

---
+++ src/main/java/com/example/jenkinsdemo/TestingController.java
@@ -0,0 +1,14 @@
+package com.example.jenkinsdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestingController {
+
+
+    @GetMapping("/test")
+    public String test() {
+        return "running...";
+    }
+}
